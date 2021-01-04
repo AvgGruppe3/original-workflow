723d27fbce7d366fd97d480850db1cc21314f919
Philipp <deph1015@hs-karlsruhe.de>
2021-01-04 13:10:43 +0100

    some changes

---
+++ src/main/java/de/hska/acme/adapter/KundeInformieren.java
@@ -0,0 +1,12 @@
+package de.hska.acme.adapter;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class KundeInformieren implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution execution) {
+        execution.setVariable("text",
+                "Ihr Antrag ist unvollständig, bitte füllen Sie den Antrag vollständig aus");
+    }
+}
---
+++ src/main/java/de/hska/acme/adapter/LokalSpeichern.java
@@ -0,0 +1,11 @@
+package de.hska.acme.adapter;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class LokalSpeichern implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+
+    }
+}
